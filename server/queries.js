3ab14286c39cf009cd4a16d7350d89042d9b3f5d
SandraM1 <sministro@gmail.com>
2021-08-15 20:57:07 -0400

    completed post review query

--- server/queries.js
+++ server/queries.js
@@ -1,5 +1,7 @@
 const pool = require('../db/index.js');
 
+
+
 const getReviews = (parameters, callback) => {
   let productID = parameters.product_id;
   let count = parameters.count || 5;
@@ -16,7 +18,12 @@ const getReviews = (parameters, callback) => {
     orderedBy = 'date DESC, helpfulness DESC'
   }
 
-  let queryString = `SELECT review.id, rating, summary, recommend, response, body, date, reviewer_name, helpfulness, ARRAY_AGG (json_build_object('id', reviewPhotos.id, 'url' , url)) as photos FROM review LEFT JOIN reviewPhotos ON review_id = review.id WHERE product_id = ${productID} GROUP BY review.id ORDER BY ${orderedBy} LIMIT ${count}`;
+  // REPORTED can be false or null   --- do not send back if reported is true
+
+  let queryString = `SELECT review.id, rating, summary, recommend, reported, response, body, date, reviewer_name, helpfulness,
+    ARRAY_AGG (json_build_object('id', reviewPhotos.id, 'url' , url)) as photos FROM review
+    LEFT JOIN reviewPhotos ON review_id = review.id WHERE product_id = ${productID} GROUP BY review.id
+    ORDER BY ${orderedBy} LIMIT ${count}`;
 
   let finalResult = {
     "product": `${productID}`,
@@ -35,6 +42,10 @@ const getReviews = (parameters, callback) => {
     });
   }).then((results) => {
     console.log('USER GET promise resolved');
+    // results.rows --- an array of all matching rows based on query
+    // results.fields --- all fields of table queried
+    console.log('results from getting reviews', results.rows);
+
     finalResult['results'] = results.rows;
     callback(null, finalResult);
   }).catch((err) => {
@@ -42,8 +53,203 @@ const getReviews = (parameters, callback) => {
   });
 }
 
+
+
+const postReview = (addedReview, callback) => {
+  console.log('adding this review', addedReview)
+  let productID = addedReview.product_id;
+  let rating = addedReview.rating;
+  let summary = addedReview.summary;
+  let body = addedReview.body;
+  let recommend = addedReview.recommend;
+  let name = addedReview.name;
+  let email = addedReview.email;
+  let url = addedReview.photos[0]; // this is an array of url strings
+  let allChars = Object.keys(addedReview.characteristics);
+  let charID =allChars[0]; // this is the first key of the obj - which represents char_id
+  let charRatingValue = addedReview.characteristics[charID];
+
+  let queryString = `WITH newReview AS (
+      INSERT INTO review
+      (product_id, rating, summary, body, recommend, reported, reviewer_name, reviewer_email, response, helpfulness)
+      VALUES
+      ('${productID}', '${rating}', '${summary}', '${body}', '${recommend}', false, '${name}', '${email}', null, 0) RETURNING id),
+    addPhotos AS (
+      INSERT INTO reviewPhotos (review_id, url)
+      SELECT id, '${url}' FROM newReview RETURNING *),
+    addChars AS (
+      INSERT INTO characteristic_reviews(characteristic_id, review_id, value)
+      SELECT '${charID}', id, '${charRatingValue}' FROM newReview RETURNING *)
+      SELECT * FROM addChars`;
+
+  return new Promise((resolve, reject) => {
+    pool.query(queryString, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  }).then((results) => {
+    console.log('USER POST promise resolved');
+    // console.log('results from posting a review', results)
+    callback(null, results);
+  }).catch((err) => {
+    callback(err, null);
+  });
+}
+
+
+
+const getMetaData = (parameter, callback) => {
+  let productID = parameter.product_id;
+  let queryString = `SELECT rating, recommend FROM review WHERE product_id = ${productID}`; // given a productID - returns an object
+
+  let queryTwo = `WITH first AS (
+    SELECT id, name FROM characteristics WHERE product_id = ${productID} RETURNING id, name),
+    second AS (
+      SELECT value FROM characteristic_reviews WHERE characteristic_id = id FROM first RETURNING value
+    )`;
+
+  let finalResult = {
+    "product_id": `${productID}`,
+    "ratings": {          /// ratings is in review table -- get rating from all reviews table -- get average for each rating
+    },
+    "recommended": {        /// recommended is in review table
+      "false": 0,
+      "true": 0
+    },
+    "characteristics": {    /// select all char id & names given product_id in chars table ----  search char_reviews table given each id
+                                                                                              // get average of all value rating to get final value count
+      "Fit": {
+        "id": "char_id",
+        "value": "avg of all values"
+      },
+      "Length": {
+        "id": "char_id",
+        "value": "avg of all values"
+      },
+      "Comfort": {
+        "id": "char_id",
+        "value": "avg of all values" // ---varchar datatype
+      },
+    }
+  }
+
+
+
+  return new Promise((resolve, reject) => {
+    pool.query(queryTwo, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  }).then((results) => {
+    let firstQuery = results.rows; // an array of objs
+    console.log('what is this first', firstQuery)
+
+    // firstQuery.forEach((item) => {
+    //   if (!finalResult['ratings'][item.rating]) { // if not currently in obj, begin count
+    //     finalResult['ratings'][item.rating] = 1;
+    //   } else {
+    //     finalResult['ratings'][item.rating]++;
+    //   }
+    //   if (item.recommend === true) {
+    //     finalResult['recommended']['true']++;
+    //   }
+    //   if (item.recommend === false) {
+    //     finalResult['recommended']['false']++;
+    //   }
+    // })
+
+    // pool.query(queryTwo, (err, results) => {
+    //   if (err) {
+    //     console.log('err', err);
+    //   } else {
+    //     callback(null, results);
+    //   }
+    // })
+
+
+
+    callback(null, results);
+  }).catch((err) => {
+    callback(err, null);
+  });
+}
+
+
+
+const markHelpful = (reviewID, callback) => {
+  // increment helpfulness int
+
+  let queryString = 'UPDATE * FROM review WHERE product_id = 4';
+
+  return new Promise((resolve, reject) => {
+    pool.query(queryString, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  }).then((results) => {
+    console.log('USER GET promise resolved');
+    console.log('results from photos', results)
+    callback(null, results);
+  }).catch((err) => {
+    callback(err, null);
+  });
+}
+
+
+
+
+const markReported = (reviewID, callback) => {
+  // update reported field to true
+
+  let queryString = 'UPDATE * FROM review WHERE product_id = 4';
+
+  return new Promise((resolve, reject) => {
+    pool.query(queryString, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+  }).then((results) => {
+    console.log('USER GET promise resolved');
+    console.log('results from photos', results)
+    callback(null, results);
+  }).catch((err) => {
+    callback(err, null);
+  });
+}
+
+
+
 const getPhotos = (callback) => {
-  let queryString = 'SELECT * FROM reviewPhotos WHERE review_id = 2283091';
+  // let queryString = 'SELECT * FROM review WHERE product_id = 4';
+  // let queryString = 'SELECT * FROM characteristics WHERE product_id = 37311';
+  // let queryString = 'SELECT * FROM review WHERE id = 5774952';
+
+  // let queryString = 'SELECT * FROM review WHERE id = 5774956';
+  // my test one with ian but not inserting into photos/chars table
+
+  // let queryString = 'SELECT * FROM reviewPhotos WHERE review_id = 5774963';
+
+  // let queryString = 'SELECT * FROM review WHERE id = 5774963';
+
+  let photos = ["https://source.unsplash.com/random", "https://source.unsplash.com/random"];
+
+  // let queryString = `INSERT INTO reviewPhotos (review_id, url) VALUES (5774963, 'https://source.unsplash.com/random')`; --- this works!
+  // let queryString = `INSERT INTO reviewPhotos (review_id, url) VALUES (5774963, photo::text[] FROM unnest(${photos}::text[]) AS t (photo))`; --- this does not work
+  let queryString = `INSERT INTO reviewPhotos (review_id, url)
+  VALUES (5774963, unnest('${photos}'::text[])
+  )`;
 
   return new Promise((resolve, reject) => {
     pool.query(queryString, (err, results) => {
@@ -62,27 +268,13 @@ const getPhotos = (callback) => {
   });
 }
 
-const postReview = (callback) => {
-  let queryString = 'SELECT * FROM reviewPhotos WHERE review_id = 2283091';
 
-  return new Promise((resolve, reject) => {
-    pool.query(queryString, (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results);
-      }
-    });
-  }).then((results) => {
-    console.log('USER GET promise resolved');
-    console.log('results from photos', results)
-    callback(null, results);
-  }).catch((err) => {
-    callback(err, null);
-  });
-}
 
 module.exports = {
   getReviews: getReviews,
-  getPhotos: getPhotos
+  postReview: postReview,
+  getMetaData: getMetaData,
+  getPhotos: getPhotos,
+  markHelpful: markHelpful,
+  markReported: markReported
 }
--- server/server.js
+++ server/server.js
@@ -5,13 +5,13 @@ const path = require('path');
 const app = express();
 
 const pool = require('../db/index.js');
-const { getReviews, getPhotos } = require('./queries.js');
+const { getReviews, postReview, getMetaData, getPhotos, markHelpful, markReported } = require('./queries.js');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).send('Success!');
-})
+});
 
 
 // user sends get request --> /reviews?product_id=1
@@ -20,7 +20,6 @@ app.get('/', (req, res) => {
 
 app.get('/reviews', (req, res) => {
   let queryParams = req.query;
-  console.log('one', queryParams); // { product_id: '1' }
 
   if (!queryParams.product_id) {
     res.status(422).send('Error: invalid product_id provided');
@@ -31,23 +30,95 @@ app.get('/reviews', (req, res) => {
         console.log('Error retrieving messages ' + err);
         res.sendStatus(404);
       } else {
-        // console.log('I got some results back', results);
-        // results.rows --- an array of all matching rows based on query
-        // results.fields --- all fields of table queried
         res.status(200).json(results);
       }
     });
 
   }
-})
+});
 
 app.post('/reviews', (req,res) => {
-  res.status(201).send('Posted!')
+  let newReview = req.body;
+
+  postReview(newReview, (err, results) => {
+    if (err) {
+      console.log('Error retrieving messages ' + err);
+      res.sendStatus(404);
+    } else {
+      res.status(201).send('Posted!');
+    }
+  });
+
+});
+
+
+app.get('/reviews/meta', (req, res) => {
+
+  let queryParams = req.query; // user will send product_id
+  console.log('meta data req', queryParams)
+
+  if (!queryParams.product_id) {
+    res.status(422).send('Error: invalid product_id provided');
+  } else {
+
+    getMetaData(queryParams, (err, results) => {
+      if (err) {
+        console.log('Error retrieving messages ' + err);
+        res.sendStatus(404);
+      } else {
+        res.status(200).json(results);
+      }
+    });
+
+  }
+
+});
+
+
+app.put('/reviews/review_id/helpful', (req, res) => {
+  let reviewID = req.params; // ?
+
+  markHelpful(reviewID, (err, results) => {
+    if (err) {
+      console.log('Error retrieving messages ' + err);
+      res.sendStatus(404);
+    } else {
+      res.status(200).send('Review marked helpful!');
+    }
+  });
+})
+
+
+app.put('/reviews/review_id/report', (req, res) => {
+  let reviewID = req.params; // ?
+
+  markHelpful(reviewID, (err, results) => {
+    if (err) {
+      console.log('Error retrieving messages ' + err);
+      res.sendStatus(404);
+    } else {
+      res.status(200).send('Review has been reported!');
+    }
+  });
+})
+
+
+app.get('/reviews/test', (req, res) => {
+
+  getPhotos((err, results) => {
+    if (err) {
+      console.log('Error retrieving messages ' + err);
+      res.sendStatus(404);
+    } else {
+      res.status(200).json(results);
+    }
+  });
+
 })
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
-})
+});
 
 
 
